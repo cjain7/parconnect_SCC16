a10d8142c20799efe7b87669643a36c45e30c20e
Chirag Jain <cjain7@gatech.edu>
2016-04-09 14:54:15 -0400

    Pointer doubling enabled

--- src/coloring/labelProp.hpp
+++ src/coloring/labelProp.hpp
@@ -52,7 +52,7 @@ namespace conn
      * @tparam[in]  DOUBLING      controls whether pointer doubling would be executed or not, 'ON' by default.
      * @tparam[in]  OPTIMIZATION  optimization level for benchmarking, use loadbalanced for the best version 
      */
-    template<typename nIdType = uint64_t, uint8_t DOUBLING = lever::OFF, uint8_t OPTIMIZATION = opt_level::loadbalanced>
+    template<typename nIdType = uint64_t, uint8_t DOUBLING = lever::ON, uint8_t OPTIMIZATION = opt_level::loadbalanced>
     class ccl 
     {
       public:
